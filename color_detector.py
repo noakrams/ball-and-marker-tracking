a8f70a074e95ae9dbca90572d1182f37e02b3818
noakrams <noakrams96@gmail.com>
2021-03-07 14:42:36 +0200

    Final.
    Signed-off-by: noakrams <noakrams96@gmail.com>

--- color_detector.py
+++ color_detector.py
@@ -1,103 +1,40 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# USAGE: You need to specify a filter and "only one" image source
-#
-# (python) range-detector --filter RGB --image /path/to/image.png
-# or
-# (python) range-detector --filter HSV --webcam
-
 import cv2
-import argparse
-from operator import xor
+import numpy as np
 
-
-def callback(value):
+def empty(a):
     pass
 
-
-def setup_trackbars(range_filter):
-    cv2.namedWindow("Trackbars", 0)
-
-    for i in ["MIN", "MAX"]:
-        v = 0 if i == "MIN" else 255
-
-        for j in range_filter:
-            cv2.createTrackbar("%s_%s" % (j, i), "Trackbars", v, 255, callback)
-
-
-def get_arguments():
-    ap = argparse.ArgumentParser()
-    ap.add_argument('-f', '--filter', required=True,
-                    help='Range filter. RGB or HSV')
-    ap.add_argument('-i', '--image', required=False,
-                    help='Path to the image')
-    ap.add_argument('-w', '--webcam', required=False,
-                    help='Use webcam', action='store_true')
-    ap.add_argument('-p', '--preview', required=False,
-                    help='Show a preview of the image after applying the mask',
-                    action='store_true')
-    args = vars(ap.parse_args())
-
-    if not xor(bool(args['image']), bool(args['webcam'])):
-        ap.error("Please specify only one image source")
-
-    if not args['filter'].upper() in ['RGB', 'HSV']:
-        ap.error("Please speciy a correct filter.")
-
-    return args
-
-
-def get_trackbar_values(range_filter):
-    values = []
-
-    for i in ["MIN", "MAX"]:
-        for j in range_filter:
-            v = cv2.getTrackbarPos("%s_%s" % (j, i), "Trackbars")
-            values.append(v)
-
-    return values
-
-
 def main():
-    args = get_arguments()
-
-    range_filter = args['filter'].upper()
-
-    if args['image']:
-        image = cv2.imread(args['image'])
-
-        if range_filter == 'RGB':
-            frame_to_thresh = image.copy()
-        else:
-            frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    else:
-        camera = cv2.VideoCapture(0)
-
-    setup_trackbars(range_filter)
+    camera = cv2.VideoCapture(0)
+    filter_type = 'HSV'
+    cv2.namedWindow('Trackbars', cv2.WINDOW_NORMAL)
+    cv2.createTrackbar("Hue Min", "Trackbars", 0, 255, empty)
+    cv2.createTrackbar("Sat Min", "Trackbars", 0, 255, empty)
+    cv2.createTrackbar("Val Min", "Trackbars", 0, 255, empty)
+    cv2.createTrackbar("Hue Max", "Trackbars", 255, 255, empty)
+    cv2.createTrackbar("Sat Max", "Trackbars", 255, 255, empty)
+    cv2.createTrackbar("Val Max", "Trackbars", 255, 255, empty)
 
     while True:
-        if args['webcam']:
-            ret, image = camera.read()
+        ret, image = camera.read()
 
-            if not ret:
-                break
+        if not ret:
+            break
 
-            if range_filter == 'RGB':
-                frame_to_thresh = image.copy()
-            else:
-                frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        imgHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        h_min = cv2.getTrackbarPos("Hue Min", "Trackbars")
+        s_min = cv2.getTrackbarPos("Sat Min", "Trackbars")
+        v_min = cv2.getTrackbarPos("Val Min", "Trackbars")
+        h_max = cv2.getTrackbarPos("Hue Max", "Trackbars")
+        s_max = cv2.getTrackbarPos("Sat Max", "Trackbars")
+        v_max = cv2.getTrackbarPos("Val Max", "Trackbars")
 
-        v1_min, v2_min, v3_min, v1_max, v2_max, v3_max = get_trackbar_values(range_filter)
+        lower = np.array([h_min, s_min, v_min])
+        upper = np.array([h_max, s_max, v_max])
+        mask = cv2.inRange(imgHSV, lower, upper)
 
-        thresh = cv2.inRange(frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max))
-
-        if args['preview']:
-            preview = cv2.bitwise_and(image, image, mask=thresh)
-            cv2.imshow("Preview", preview)
-        else:
-            cv2.imshow("Original", image)
-            cv2.imshow("Thresh", thresh)
+        cv2.imshow("Original", image)
+        cv2.imshow("Mask", mask)
 
         if cv2.waitKey(1) & 0xFF is ord('q'):
             break
@@ -105,3 +42,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
--- track.py
+++ track.py
@@ -1,4 +1,5 @@
-# import the necessary packages
+#!/usr/bin/python3
+
 from collections import deque
 from imutils.video import VideoStream
 import numpy as np
@@ -7,13 +8,12 @@ import cv2
 import imutils
 import time
 
+# hough line detector with cv2
 def findColorAndLine(hsv, frame, markerColors):
-	masks = []
 	for idxColor, color in enumerate(markerColors):
 		lower = np.array(color[0:3])
 		upper = np.array(color[3:6])
 		mask = cv2.inRange(hsv, lower, upper)
-		masks.append(mask)
 		edges_line = cv2.Canny(mask, 75, 150)
 		lines = cv2.HoughLinesP(edges_line, 1, np.pi/180, 50, maxLineGap=50)
 		if lines is not None:
@@ -24,17 +24,17 @@ def findColorAndLine(hsv, frame, markerColors):
 					trail_points[idxColor].appendleft((int(x2) , int(y2)))
 				else:
 					trail_points[idxColor].appendleft((int(x1), int(y1)))
-	return masks
 
 # hough circles with cv2
 def findBall(hsv, ballMin, ballMax, pts_ball):
 	tennis_mask = cv2.inRange(hsv, ballMin, ballMax)
 	tennis_mask = cv2.dilate(tennis_mask, None, iterations=2)
 	tennis_mask = cv2.erode(tennis_mask, None, iterations=2)
+	ball_edges = cv2.Canny(tennis_mask, 75, 150)
 	size = tennis_mask.shape
 	h, s = 0.0, 0.0
-	ball_circles = cv2.HoughCircles(tennis_mask, cv2.HOUGH_GRADIENT, 1,
-	80, param1=50, param2=10, minRadius=50, maxRadius=80)
+	ball_circles = cv2.HoughCircles(ball_edges, cv2.HOUGH_GRADIENT, 1,
+	80, param1=50, param2=10, minRadius=20, maxRadius=50)
 	if ball_circles is not None:
 		ball_circles = np.uint16(np.around(ball_circles))
 		for circle in ball_circles[0, :]:
@@ -43,35 +43,29 @@ def findBall(hsv, ballMin, ballMax, pts_ball):
 					if 0 <= (circle[1] + m) < size[0] and 0 <= (circle[0] + n) < size[1]:
 						h += tennis_mask[circle[1]+m, circle[0]+n]
 						s += 1
-			tmp = h / s
-			print(tmp)
-			if h / s > 254 and circle[2] > 50:
+			if h / s > 215 and circle[2] > 20:
 				cv2.circle(frame, (int(circle[0]), int(circle[1])), int(circle[2]), (255, 0, 0), 2)
 				center = (int(circle[0]), int(circle[1]))
 				pts_ball.appendleft(center)
-	return tennis_mask
+	return ball_edges
 
 
+ballMin = np.array([18, 64, 57])
+ballMax = np.array([40, 255, 255])
 
+""" Change the values according to the mask created by "color_detector" file in the following order:
+	Hue min, Sat min, Val min, Hue max, Sat max, Val max.
+	In my case, the first is the value of orange and the second is blue."""
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-t", "--trail", type = int, default = 50, help="max trail size")
-args = vars(ap.parse_args())
-ballMin = np.array([17, 15, 18])
-ballMax = np.array([76, 255, 255])
-markerColors = [[0, 80, 183, 27, 255, 248],  # 0, 138, 157, 250, 255, 255
-				[50, 112, 134, 250, 255, 255]] #orange, blue
-myColors = [[0, 165, 255], [255, 165, 0]] #orange, blue
-pts_ball = deque(maxlen = args["trail"])
-trail_points = [deque(maxlen = args["trail"]), deque(maxlen = args["trail"])]
-
+markerColors = [[0, 29, 250, 43, 255, 255],  
+				[50, 170, 186, 255, 255, 255]] # orange, blue
+myColors = [[0, 165, 255], [255, 165, 0]] # orange, blue
+pts_ball = deque(maxlen = 80)
+trail_points = [deque(maxlen = 80), deque(maxlen = 80)]
 	
 cap = cv2.VideoCapture(0)
 cap.set(3, 600)
 cap.set(4, 480)
-
-# allow the camera or video file to warm up
-time.sleep(2.0)
 
 while True:
 	ret, frame = cap.read()
@@ -83,34 +77,28 @@ while True:
 	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
 	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-	tennis_mask = findBall(hsv, ballMin, ballMax, pts_ball)
+	ball_edges = findBall(hsv, ballMin, ballMax, pts_ball)
 
-	# Draw tennis ball 	
+	# Draw balls trail
 	for i in range(1, len(pts_ball)):
 		if pts_ball[i - 1] is None or pts_ball[i] is None:
 			continue
-		thickness = int(np.sqrt(args["trail"] / float(i + 1)) * 1.9)
-		cv2.line(frame, pts_ball[i - 1], pts_ball[i], (0, 255, 255), thickness)
+		trails_size = int(np.sqrt(80 / float(i + 1)) * 1.9)
+		cv2.line(frame, pts_ball[i - 1], pts_ball[i], (0, 255, 255), trails_size)
 
-	masks = findColorAndLine(hsv, frame, markerColors)
-	
-	# Draw marker's trail
+	findColorAndLine(hsv, frame, markerColors)
+
+	# Draw markers trail
 	for idx, dq_pts in enumerate(trail_points): 
 		for i in range(1, len(dq_pts)):
 			if dq_pts[i - 1] is None or dq_pts[i] is None:
 				continue
-			thickness = int(np.sqrt(args["trail"] / float(i + 1)) * 1.9)
-			cv2.line(frame, dq_pts[i - 1], dq_pts[i], myColors[idx], thickness)
+			trails_size = int(np.sqrt(80 / float(i + 1)) * 1.9)
+			cv2.line(frame, dq_pts[i - 1], dq_pts[i], myColors[idx], trails_size)
 
-	if masks is not None:
-			cv2.imshow("orange", masks[0])
-			cv2.imshow("blue", masks[1])
-    				
-	cv2.imshow("tennis_mask", tennis_mask)
-	# cv2.imshow("marker_mask", marker_mask)
+
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
-	# if the 'q' key is pressed, stop the loop
 	if key == ord("q"):
 		break
 
